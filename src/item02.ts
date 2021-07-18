9007cde0fd19c7e528273cb3fe20e95b28e7822b
Will <kode15333@gmail.com>
2021-07-19 08:25:10 +0900

    item02

---
+++ src/item02.ts
@@ -0,0 +1,14 @@
+export {};
+
+// noImplicitAny : Any 안되!
+function add(a: number, b: number) {
+    return a + b;
+}
+
+// strickNullChecks: Null 안되!
+add(10, null);
+
+const x: number = null;
+
+const el = document.getElementById("status");
+el.textContent = 'Ready';
