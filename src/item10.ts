9b45ac4026f6b888b9d09243ed5417084aae7e50
Will <kode15333@gmail.com>
2021-07-22 08:49:34 +0900

    :pencil2: item10

---
+++ src/item10.ts
@@ -0,0 +1,12 @@
+function getStringLen(foo: String) {
+    return foo.length;
+}
+
+getStringLen("hello");
+getStringLen(new String("hello"));
+
+function isGeeting(pharse: String) {
+    return ["hello", "good day"].includes(pharse);
+}
+
+
